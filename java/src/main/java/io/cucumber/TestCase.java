e2fcd1af7f85cab2514fd9cf6de8e5ad76683f82
Ilan Pillemer <ilanpillemer@Ilans-MBP.home>
2016-04-20 22:48:18 +0100

    Refactored hasPassed to use Streams

--- java/src/main/java/io/cucumber/TestCase.java
+++ java/src/main/java/io/cucumber/TestCase.java
@@ -9,38 +9,36 @@ import java.util.function.Predicate;
 
 public class TestCase {
     private final Map<PickleStep,List<StepDefinition>> glueySteps;
-    private Pickle pickle;
+    private final Pickle pickle;
+
+    private static Predicate<PickleStep> not(Predicate<PickleStep> p) {
+	return p.negate();
+    }
 
     public TestCase(Pickle pickle, Map<PickleStep, List<StepDefinition>> glueySteps) {
 	this.pickle = pickle;
 	this.glueySteps = glueySteps;
     }
 
-    // each step should map to one and only one lamda
-    // if there is more than one lamda there are ambiguous step definitions
-    // if there are no lamdas the step definition is undefined
     public boolean hasPassed() {
-
-	if (pickle.getSteps().stream().anyMatch(p -> this.glueySteps.get(p).size() > 1)) return false;
-	if (pickle.getSteps().stream().anyMatch(p -> this.glueySteps.get(p).size() == 0)) return false;
+	if (pickle.getSteps().stream().anyMatch(isAmbiguous())) return false;
+	if (pickle.getSteps().stream().anyMatch(isUndefined())) return false;
 	try {
-	pickle.getSteps().stream().
-	    filter(p -> this.glueySteps.get(p).size() < 2).
-	    filter(p -> this.glueySteps.get(p).size() > 0 ).
-	    forEach( (PickleStep p) -> this.glueySteps.get(p).get(0).getLamda().call() );
+	    pickle.getSteps().stream().
+		filter(not(isAmbiguous())).
+		filter(not(isUndefined())).
+		forEach( p -> glueySteps.get(p).get(0).getLamda().call() );
 	} catch (RuntimeException e) {
 	    return false;
 	}
-	
 	return true;
     }
 
     private Predicate<PickleStep> isAmbiguous() {
-	return p -> this.glueySteps.get(p).size() > 1 ;
+	return p -> glueySteps.get(p).size() > 1 ;
     }
 
     private Predicate<PickleStep> isUndefined() {
 	return p -> glueySteps.get(p).size() == 0 ;
     }
-    
 }
