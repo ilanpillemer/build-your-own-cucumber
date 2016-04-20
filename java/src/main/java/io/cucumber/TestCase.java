a9ad21161723a21a83a12526e14bfc079ce58a32
Ilan Pillemer <ilanpillemer@Ilans-MBP.home>
2016-04-20 22:29:28 +0100

    got back to green with some major refactor
    still needs some more work

--- java/src/main/java/io/cucumber/TestCase.java
+++ java/src/main/java/io/cucumber/TestCase.java
@@ -5,10 +5,10 @@ import gherkin.pickles.PickleStep;
 
 import java.util.List;
 import java.util.Map;
-
+import java.util.function.Predicate;
 
 public class TestCase {
-    private Map<PickleStep,List<StepDefinition>> glueySteps;
+    private final Map<PickleStep,List<StepDefinition>> glueySteps;
     private Pickle pickle;
 
     public TestCase(Pickle pickle, Map<PickleStep, List<StepDefinition>> glueySteps) {
@@ -20,14 +20,27 @@ public class TestCase {
     // if there is more than one lamda there are ambiguous step definitions
     // if there are no lamdas the step definition is undefined
     public boolean hasPassed() {
-	for (PickleStep ps : pickle.getSteps()) {
-	    if (glueySteps.get(ps).size() != 1) return false; 
-	    try {
-		glueySteps.get(ps).get(0).getLamda().call();
-	    } catch (RuntimeException e) {
-		return false;
-	    }
+
+	if (pickle.getSteps().stream().anyMatch(p -> this.glueySteps.get(p).size() > 1)) return false;
+	if (pickle.getSteps().stream().anyMatch(p -> this.glueySteps.get(p).size() == 0)) return false;
+	try {
+	pickle.getSteps().stream().
+	    filter(p -> this.glueySteps.get(p).size() < 2).
+	    filter(p -> this.glueySteps.get(p).size() > 0 ).
+	    forEach( (PickleStep p) -> this.glueySteps.get(p).get(0).getLamda().call() );
+	} catch (RuntimeException e) {
+	    return false;
 	}
+	
 	return true;
     }
+
+    private Predicate<PickleStep> isAmbiguous() {
+	return p -> this.glueySteps.get(p).size() > 1 ;
+    }
+
+    private Predicate<PickleStep> isUndefined() {
+	return p -> glueySteps.get(p).size() == 0 ;
+    }
+    
 }
